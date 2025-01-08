3a5135c45b7dcc16323aa429af33627984e1f0b2
ecthelionofthefountain-510 <lundstedt.kevin@gmail.com>
2025-01-08 10:20:32 +0100

    added shit files

--- obj/Debug/net8.0/Wordapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Wordapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wordapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511fcbcac599c69c5e1cbe6ff263563ae63f5bcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2ec89f248c05f009baa8dc05390b5c79f025a1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wordapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wordapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
